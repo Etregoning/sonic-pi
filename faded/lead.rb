dd0adaff38b2ec928819d8203edb39fb10e2181d
Elliot Tregoning <etregoni@e1z3r7p2.42.us.org>
2017-11-14 22:30:15 -0800

    made changes to the song

--- faded/lead.rb
+++ faded/lead.rb
@@ -1,89 +1,93 @@
 #Lead Buffer
 # Measure 1 - 8
 define :lead do
-  measure1 = [:Fs5, :Fs5, :Fs5, :Fs5, :Fs5, :Fs5, :Fs5, :As5, :As5, :As5]
-  release1 = [1, 1, 1, 1, 1, 1, 0.5, 1, 0.5, 1]
-  timing1 = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.25, 0.25, 0.25]
-  measure2 = [:Ds6, :Ds6, :Ds6, :Ds6, :Ds6, :Ds6, :Cs6, :Cs6]
-  
-  2.times do
-    measure1.zip(timing1, release1).each do |m, t, r|
-      play m, release: r
-      sleep t
-    end
-    measure2.each do |m|
-      play m
-      sleep 0.5
-    end
-    timing1.each do |t|
-      play :As5
-      sleep t
-    end
-    8.times do
-      play :F5
-      sleep 0.5
+  use_synth :prophet
+  use_synth_defaults note: 60, amp: 0.4
+  with_fx :reverb do
+    measure1 = [:Fs5, :Fs5, :Fs5, :Fs5, :Fs5, :Fs5, :Fs5, :As5, :As5, :As5]
+    release1 = [1, 1, 1, 1, 1, 1, 0.5, 1, 0.5, 1]
+    timing1 = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.25, 0.25, 0.25]
+    measure2 = [:Ds6, :Ds6, :Ds6, :Ds6, :Ds6, :Ds6, :Cs6, :Cs6]
+    
+    2.times do
+      measure1.zip(timing1, release1).each do |m, t, r|
+        play m, release: r
+        sleep t
+      end
+      measure2.each do |m|
+        play m
+        sleep 0.5
+      end
+      timing1.each do |t|
+        play :As5
+        sleep t
+      end
+      8.times do
+        play :F5
+        sleep 0.5
+      end
     end
   end
   lead9_to_11
 end
 
 define :lead9_to_11 do
-  measure9 = [:Fs5, :Fs5, :Fs5, :Fs5, :AS5, :As5, :Fs6, :Fs6, :F6, :F6]
-  release9 = [1, 1, 1, 1, 1, 0.5, 1, 1, 1]
-  timing9 = [0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.5, 0.25, 0.25, 0.25]
-  measure10 = [:Ds6, :Ds6, :Ds6, :Cs6, :Cs6, :Cs6, :Gs5, :Gs5]
-  measure11 = [:As5, :As5, :As5, :As5, :Cs6, :B5, :As5, :Gs5, :Fs5, :F5]
-  release11 = [1, 1, 1, 1, 1, 0.5, 0.25, 0.5, 0.5, 0.5]
-  timing11 = [0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.25, 0.25, 0.25, 0.25]
-  
-  measure9.zip(timing9, release9).each do |m, t, r|
-    play m, release: r
-    sleep t
-  end
-  measure10.each do |m|
-    play m
-    sleep 0.5
-  end
-  measure11.zip(timing11, release11).each do |m, t, r|
-    play m, release: r
-    sleep t
+  use_synth :prophet
+  use_synth_defaults note: 60, amp: 0.4
+  with_fx :reverb do
+    measure9 = [:Fs5, :Fs5, :Fs5, :Fs5, :AS5, :As5, :Fs6, :Fs6, :F6, :F6]
+    release9 = [1, 1, 1, 1, 1, 0.5, 1, 1, 1]
+    timing9 = [0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.5, 0.25, 0.25, 0.25]
+    measure10 = [:Ds6, :Ds6, :Ds6, :Cs6, :Cs6, :Cs6, :Gs5, :Gs5]
+    measure11 = [:As5, :As5, :As5, :As5, :Cs6, :B5, :As5, :Gs5, :Fs5, :F5]
+    release11 = [1, 1, 1, 1, 1, 0.5, 0.25, 0.5, 0.5, 0.5]
+    timing11 = [0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.25, 0.25, 0.25, 0.25]
+    
+    measure9.zip(timing9, release9).each do |m, t, r|
+      play m, release: r
+      sleep t
+    end
+    measure10.each do |m|
+      play m
+      sleep 0.5
+    end
+    measure11.zip(timing11, release11).each do |m, t, r|
+      play m, release: r
+      sleep t
+    end
   end
 end
 
 define :lead12_to_16 do
-  measure12 = [:F5, :F5, :F5, :F5, :Gs5, :F5]
-  release12 = [1, 1, 1, 1, 2, 2]
-  timing12 = [0.5, 0.5, 0.5, 0.5, 1, 1]
-  measure16 = [:F5, :F5, :F5, :F5, :Fs5, :Gs5, :Fs5]
-  release16 = [1, 1, 1, 1, 2, 1, 1]
-  timing16 =  [0.5, 0.5, 0.5, 0.5, 1, 0.5, 0.5]
-  measure12.zip(timing12, release12).each do |m, t, r|
-    play m, release: r
-    sleep t
-  end
-  lead9_to_11
-  measure16.zip(timing16, release16).each do |m, t, r|
-    play m, release: r
-    sleep t
+  use_synth :prophet
+  use_synth_defaults note: 60, amp: 0.4
+  with_fx :reverb do
+    measure12 = [:F5, :F5, :F5, :F5, :Gs5, :F5]
+    release12 = [1, 1, 1, 1, 2, 2]
+    timing12 = [0.5, 0.5, 0.5, 0.5, 1, 1]
+    measure16 = [:F5, :F5, :F5, :F5, :Fs5, :Gs5, :Fs5]
+    release16 = [1, 1, 1, 1, 2, 1, 1]
+    timing16 =  [0.5, 0.5, 0.5, 0.5, 1, 0.5, 0.5]
+    measure12.zip(timing12, release12).each do |m, t, r|
+      play m, release: r
+      sleep t
+    end
+    lead9_to_11
+    measure16.zip(timing16, release16).each do |m, t, r|
+      play m, release: r
+      sleep t
+    end
   end
 end
 
 
 define :start_lead do
-  use_synth :pretty_bell
-  use_synth_defaults note: 60, amp: 0.4
-  use_bpm 90
-  with_fx :reverb do
-    lead
-    lead12_to_16
-  end
+  drum_snare
+  sleep 1
+  bass
+  lead
+  lead12_to_16
 end
-
-
-
-start_lead
-
-
 
 
 
--- faded/piano.rb
+++ faded/piano.rb
@@ -1,7 +1,60 @@
 # Entry point + piano buffer
 define :piano do
   use_synth :piano
-  # Arrays per measure here, most people use 1 big array, but I think this looks cleaner :)
+  with_fx :reverb do
+    # Arrays per measure here, most people use 1 big array, but I think this looks cleaner :)
+    measure1 = [[:Ds4, :Fs5], :As4, [:Fs5, :Fs4], :As4, [:Ds4, :Fs5], :As4, [:As5, :Fs4], :As4]
+    measure2 = [[:B3, :Ds6], :Fs4, [:Ds6, :Ds4], :Fs4, [:B3, :Ds6], :Fs4, [:Cs6, :Ds4], :Fs4]
+    measure3 = [[:As5, :Fs4], :Cs5, [:As5, :As4], :Cs5, [:As5, :Fs4], :Cs5, [:As5, :As4], :Cs5]
+    measure4 = [[:F5, :Cs4], :Gs4, [:F5, :F4], :Gs4, [:F5, :Cs4], :Gs4, [:Ds5, :F4], :Gs4]
+    timing = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+    
+    # Zip links measure with timing to set the beat, each loops over the joined arrays
+    with_fx :reverb do
+      measure1.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
+      measure2.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
+      measure3.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
+      measure4.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
+      play :Ds3
+      measure1.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
+      play :B2
+      measure2.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
+      play :Fs2
+      measure3.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
+      play :Cs3
+      measure4.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
+    end
+  end
+end
+
+define :guitar do
+  use_synth :pluck
+  use_synth_defaults amp: 0.2
+  use_octave 0
   measure1 = [[:Ds4, :Fs5], :As4, [:Fs5, :Fs4], :As4, [:Ds4, :Fs5], :As4, [:As5, :Fs4], :As4]
   measure2 = [[:B3, :Ds6], :Fs4, [:Ds6, :Ds4], :Fs4, [:B3, :Ds6], :Fs4, [:Cs6, :Ds4], :Fs4]
   measure3 = [[:As5, :Fs4], :Cs5, [:As5, :As4], :Cs5, [:As5, :Fs4], :Cs5, [:As5, :As4], :Cs5]
@@ -10,98 +63,37 @@ define :piano do
   
   # Zip links measure with timing to set the beat, each loops over the joined arrays
   with_fx :reverb do
-    measure1.zip(timing).each do |m, t|
-      play m
-      sleep t
-    end
-    measure2.zip(timing).each do |m, t|
-      play m
-      sleep t
-    end
-    measure3.zip(timing).each do |m, t|
-      play m
-      sleep t
-    end
-    measure4.zip(timing).each do |m, t|
-      play m
-      sleep t
-    end
-    play :Ds3
-    measure1.zip(timing).each do |m, t|
-      play m
-      sleep t
-    end
-    play :B2
-    measure2.zip(timing).each do |m, t|
-      play m
-      sleep t
-    end
-    play :Fs2
-    measure3.zip(timing).each do |m, t|
-      play m
-      sleep t
-    end
-    play :Cs3
-    measure4.zip(timing).each do |m, t|
-      play m
-      sleep t
+    2.times do
+      play :Ds3
+      measure1.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
+      play :B2
+      measure2.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
+      play :Fs2
+      measure3.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
+      play :Cs3
+      measure4.zip(timing).each do |m, t|
+        play m
+        sleep t
+      end
     end
   end
 end
 
-define :piano2 do
-  use_synth :fm
-  use_synth_defaults amp: 0.3
-  with_fx :reverb do
-    measure1 = [[:Ds5, :As4, :Ds4], [:Ds5, :As4, :Ds4], [:Ds5, :As4, :Ds4], [:Cs5, :As4, :Cs4]]
-    measure2 = [[:B4, :Fs4, :B3], [:Ds5, :Ds4, :B3], [:Ds5, :Ds4, :B3], [:Cs5, :Cs4, :B3]]
-    measure3 = [[:Cs5, :Fs4, :Cs4], [:Cs5, :Fs4, :Cs4], [:Cs5, :Fs4, :Cs4], [:Cs5, :Fs4, :Cs4]]
-    measure4 = [[:Cs5, :Gs4, :Cs4], [:Cs5, :Gs4, :Cs4], [:Cs5, :Gs4, :Cs4], [:Cs5, :Gs4, :Cs4]]
-    
-    measure1.each do |m|
-      play m, release: 1.5
-      sleep 0.75
-    end
-    measure2.each do |m|
-      play m, release: 1.5
-      sleep 0.75
-    end
-    measure3.each do |m|
-      play m, release: 1.5
-      sleep 0.75
-    end
-    measure4.each do |m|
-      play m, release: 1.5
-      sleep 0.75
-    end
-    measure1.each do |m|
-      play m, release: 1.5
-      sleep 0.75
-    end
-    measure2.each do |m|
-      play m, release: 1.5
-      sleep 0.75
-    end
-    measure3.each do |m|
-      play m, release: 1.5
-      sleep 0.75
-    end
-    measure4.each do |m|
-      play m, release: 1.5
-      sleep 0.75
-    end
-  end
+define :start_song do
+  use_bpm 90
+  piano
+  fm_piano
+  start_lead
 end
 
-piano2
 
-##| define :start_song do
-##|   use_bpm 90
-##|   piano
-##|   drums
-##|   bass
-##|   start_lead
-
-##| end
-
-##| start_song
+start_song
